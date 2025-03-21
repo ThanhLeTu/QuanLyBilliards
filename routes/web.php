82c14a300a031559a4b25a84eadf309552601f8b
Pham Van Chien <chienpham02062003@gmail.com>
2025-03-21 07:53:53 +0700

    C

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TableController;
 use App\Http\Controllers\ServiceController;
 
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // Tables routes (reference)
