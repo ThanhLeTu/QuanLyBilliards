91d3c7a5fc67875613f3852fb73e71b287a033fc
CienPham1 <pvchien.work@gmail.com>
2025-04-13 18:40:14 +0700

    Update cart item count display in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -251,7 +251,7 @@
                             <div class="card mt-4">
                                 <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
                                     <h5 class="mb-0"><i class="fas fa-shopping-cart me-2"></i>Giỏ hàng</h5>
-                                    <span class="badge bg-light text-dark" id="cartItemCount">... sản phẩm</span>
+
                                 </div>
                                 <div class="card-body p-0">
                                     <ul class="list-group list-group-flush" id="cartItems">
